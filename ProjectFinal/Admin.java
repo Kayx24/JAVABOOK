6848fc545ef9a25eaf499c5b26fd6527163f4110
duyhungdao17 <114228711+duyhungdao17@users.noreply.github.com>
2023-10-23 14:16:57 +0700

    Add files via upload

---
+++ ProjectFinal/Admin.java
@@ -0,0 +1,10 @@
+public class Admin extends TaiKhoan{
+
+    public Admin(String userName, String password, String role) {
+        super(userName, password, role);
+        
+    }
+    
+    //quyền admin
+    
+}
