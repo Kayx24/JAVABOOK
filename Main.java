4da5b89e4ee8240105f32a185b0ed532555f83f0
DiHung <daoduyhung177@gmail.com>
2023-10-30 15:56:15 +0700

    phanloai class

--- Main.java
+++ Main.java
@@ -9,7 +9,6 @@ public class Main {
         ds.themTaiKhoan("helo1", "121", "admin");
         ds.themTaiKhoan("helo2", "122", "user");
         ds.themTaiKhoan("helo3", "123", "cc");
-
         DangNhap dangNhap = new DangNhap();
         QuyenSach QS = new QuyenSach();
         // Thực hiện đăng nhập
@@ -21,8 +20,9 @@ public class Main {
         danhSachSach.add(new Sach(1, "Conan", 123, "Linh vuc A", "Loai sach A", 100, 2, "Nha xuat ban A", 2023));
         danhSachSach.add(new Sach(2, "Doraemon", 456, "Linh vuc B", "Loai sach B", 150, 3, "Nha xuat ban B", 2022));
         danhSachSach.add(new Sach(3, "One Piece", 789, "Linh vuc C", "Loai sach C", 200, 4, "Nha xuat ban C", 2021));
-    for (Sach sach : danhSachSach) {
-        System.out.println(sach);
+        for (Sach sach : danhSachSach) {
+            System.out.println(sach);
+        }
+        
     }
-}
 }
--- PhanLoai.java
+++ PhanLoai.java
@@ -1,3 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhanLoai {
-    
+    public static List<Sach> phanLoaiTheoGiaBia(List<Sach> danhSachSach, int giaBia) {
+        List<Sach> sachTheoGiaBia = new ArrayList<>();
+        for (Sach sach : danhSachSach) {
+            if (sach.getGiaBia() == giaBia) {
+                sachTheoGiaBia.add(sach);
+            }
+        }
+        return sachTheoGiaBia;
+    }
+
+    public static List<Sach> phanLoaiTheoLinhVuc(List<Sach> danhSachSach, String linhVuc) {
+        List<Sach> sachTheoLinhVuc = new ArrayList<>();
+        for (Sach sach : danhSachSach) {
+            if (sach.getTenLinhVuc().equals(linhVuc)) {
+                sachTheoLinhVuc.add(sach);
+            }
+        }
+        return sachTheoLinhVuc;
+    }
+
+    public static List<Sach> phanLoaiTheoLoaiSach(List<Sach> danhSachSach, String loaiSach) {
+        List<Sach> sachTheoLoaiSach = new ArrayList<>();
+        for (Sach sach : danhSachSach) {
+            if (sach.getTenLoaiSach().equals(loaiSach)) {
+                sachTheoLoaiSach.add(sach);
+            }
+        }
+        return sachTheoLoaiSach;
+    }
+
+    // public static void main(String[] args) {
+    //     List<Sach> danhSachSach = new ArrayList<>();
+    //     danhSachSach.add(new Sach(1, "Sach 1", 1, "LinhVuc1", "LoaiSach1", 100, 2, "NXB1", 2022));
+    //     danhSachSach.add(new Sach(2, "Sach 2", 2, "LinhVuc2", "LoaiSach2", 100, 3, "NXB2", 2020));
+    //     danhSachSach.add(new Sach(3, "Sach 3", 3, "LinhVuc1", "LoaiSach2", 120, 3, "NXB1", 2021));
+
+    //     // Phân loại sách theo giá bìa
+    //     int giaBiaCanPhanLoai = 100;
+    //     List<Sach> sachTheoGiaBia = phanLoaiTheoGiaBia(danhSachSach, giaBiaCanPhanLoai);
+    //     System.out.println("Cac sach co gia bia " + giaBiaCanPhanLoai + ":");
+    //     for (Sach sach : sachTheoGiaBia) {
+    //         System.out.println(sach);
+    //     }
+
+    //     // Phân loại sách theo lĩnh vực
+    //     String linhVucCanPhanLoai = "LinhVuc1";
+    //     List<Sach> sachTheoLinhVuc = phanLoaiTheoLinhVuc(danhSachSach, linhVucCanPhanLoai);
+    //     System.out.println("Cac sach thuoc linh vuc " + linhVucCanPhanLoai + ":");
+    //     for (Sach sach : sachTheoLinhVuc) {
+    //         System.out.println(sach);
+    //     }
+
+    //     // Phân loại sách theo loại sách
+    //     String loaiSachCanPhanLoai = "LoaiSach2";
+    //     List<Sach> sachTheoLoaiSach = phanLoaiTheoLoaiSach(danhSachSach, loaiSachCanPhanLoai);
+    //     System.out.println("Cac sach thuoc loai " + loaiSachCanPhanLoai + ":");
+    //     for (Sach sach : sachTheoLoaiSach) {
+    //         System.out.println(sach);
+    //     }
+    // }
 }
