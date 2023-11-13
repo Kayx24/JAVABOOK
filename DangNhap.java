7fc890694e348cf870c54100c56580bc77876291
KayX <truonghuytc@gmail.com>
2023-11-13 16:43:05 +0700

    sach.txt

--- DangNhap.java
+++ DangNhap.java
@@ -37,23 +37,6 @@ public class DangNhap {
         ds.docDuLieuTuFile("DanhSachTaiKhoan.txt");
 
         List<Sach> danhSachSach = new ArrayList<>();
-        danhSachSach.add(new Sach(1, "Conan","Linh vuc A", "Trinh tham", 10000, 2, "Nha xuat ban A", 2023));
-        danhSachSach.add(new Sach(2, "Doraemon","Linh vuc B", "Vui ve", 15000, 3, "Nha xuat ban B", 2022));
-        danhSachSach.add(new Sach(3, "One Piece","Linh vuc C", "Gia tuong", 20000, 4, "Nha xuat ban C", 2021));
-        danhSachSach.add(new Sach(4,"Sharelock Homes tap 1","Linh vuc D","Trinh tham",25000,5,"Nha xuat ban Kim Dong",2022));        
-        danhSachSach.add(new Sach(4,"Sharelock Homes tap 2","Linh vuc D","Trinh tham",30000,5,"Nha xuat ban Kim Dong",2022));
-        danhSachSach.add(new Sach(4,"Sharelock Homes tap 3","Linh vuc D","Trinh tham",30000,5,"Nha xuat ban Kim Dong",2022));
-        danhSachSach.add(new Sach(4,"Sharelock Homes tap 4","Linh vuc D","Trinh tham",30000,5,"Nha xuat ban Kim Dong",2022));
-        danhSachSach.add(new Sach(4,"Sharelock Homes ngoai truyen","Linh vuc D","Trinh tham",30000,5,"Nha xuat ban Kim Dong",2022));
-        danhSachSach.add(new Sach(1, "Conan","Linh vuc A", "Trinh tham", 100, 2, "Nha xuat ban A", 2023));
-        danhSachSach.add(new Sach(2, "Doraemon","Linh vuc B", "Vui ve", 150, 3, "Nha xuat ban B", 2022));
-        danhSachSach.add(new Sach(3, "One Piece","Linh vuc C", "Gia tuong", 200, 4, "Nha xuat ban C", 2021));
-        danhSachSach.add(new Sach(4,"Sharelock Homes tap 1","Linh vuc D","Trinh tham",300,5,"Nha xuat ban Kim Dong",2022));        
-        danhSachSach.add(new Sach(4,"Sharelock Homes tap 2","Linh vuc D","Trinh tham",300,5,"Nha xuat ban Kim Dong",2022));
-        danhSachSach.add(new Sach(4,"Sharelock Homes tap 3","Linh vuc D","Trinh tham",300,5,"Nha xuat ban Kim Dong",2022));
-        danhSachSach.add(new Sach(4,"Sharelock Homes tap 4","Linh vuc D","Trinh tham",300,5,"Nha xuat ban Kim Dong",2022));
-        danhSachSach.add(new Sach(4,"Sharelock Homes tap 5","Linh vuc D","Trinh tham",300,5,"Nha xuat ban Kim Dong",2022));
-        danhSachSach.add(new Sach(7,"Sach day tieng dong vat","Linh vuc E","khoa hoc",101,7,"Nha xuat ban Kim Khanh",2023));
 
         List<HoaDonItem> hoaDonItems = new ArrayList<>();
 
